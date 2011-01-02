50698f3ac5c7b1e10dea2750ce76dc8b7dc8ccaf
Cristobal Viedma <cristobal@viedma.es>
2011-01-02 20:45:58 +0100

    Fixed deleting incremental backups after a full backup

--- mysql_s3_backup/full_backup.rb
+++ mysql_s3_backup/full_backup.rb
@@ -16,8 +16,9 @@ begin
   AWS::S3::S3Object.store(File.basename(dump_file), open(dump_file), @s3_bucket)
 
   #Deleting old incremental backups (mod by cviedmai)
-  AWS::S3::Bucket.find(@s3_bucket).objects.each do |obj|
-    obj.delete if obj.key =~ /mysql-bin.[0-9]*/
+  files = AWS::S3::Bucket.find(@s3_bucket).objects.dup
+  files.each do |f|
+    AWS::S3::S3Object.delete(f.key, @s3_bucket) if f.key =~ /mysql-bin.[0-9]*/
   end
 
 ensure
